b73e74e1a748afb5be1d63fc290a52c1b396e967
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2018-01-21 19:25:23 +0000

    Tidied

--- app/src/main/java/com/example/neil/sensormonitor/AppDatabase.java
+++ app/src/main/java/com/example/neil/sensormonitor/AppDatabase.java
@@ -6,23 +6,18 @@ import android.arch.persistence.room.RoomDatabase;
 import java.util.List;
 import java.util.StringJoiner;
 
-/**
- * Created by Neil on 07/01/2018.
- */
-
 @Database(entities = {BCReading.class},version = 2, exportSchema = false)
 
 public abstract class AppDatabase extends RoomDatabase {
     public abstract BCReadingDao bcReadingDao();
 
-    public String BCSyncData(int siteId) {
+    private String BCSyncData(int siteId) {
         List<BCReading> xs = bcReadingDao().getUnsaved();
         StringJoiner sj = new StringJoiner(",");
         for(BCReading x : xs) {
             sj.add(x.toJSON(siteId).toString());
         }
-        String s = "[" + sj.toString() + "]";
-        return s;
+        return "[" + sj.toString() + "]";
     }
 }
 
